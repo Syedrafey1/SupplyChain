39b6240ed99c1c030e54e955543e130869a2ab56
Mohammad Rafey <srafey3@gmail.com>
2022-12-22 19:58:02 +0530

    Formatting UI

--- src/main/java/com/example/supplychain/HelloApplication.java
+++ src/main/java/com/example/supplychain/HelloApplication.java
@@ -2,6 +2,7 @@ package com.example.supplychain;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -24,6 +25,12 @@ public class HelloApplication extends Application {
         Button searchButton = new Button("Search");
 
         GridPane gridPane = new GridPane();
+        gridPane.setMinSize(bodyPane.getMinWidth(), headerBar-10);
+        gridPane.setVgap(5);
+        gridPane.setHgap(5);
+        gridPane.setStyle("-fx-background-color: #C0C0C0");
+
+        gridPane.setAlignment(Pos.CENTER);
 
         gridPane.add(searchText,0,0);
         gridPane.add(searchButton,1,0);
@@ -40,6 +47,12 @@ public class HelloApplication extends Application {
         PasswordField passwordField = new PasswordField();
 
         GridPane gridPane = new GridPane();
+        gridPane.setMinSize(bodyPane.getMinWidth(), bodyPane.getMinHeight());
+        gridPane.setVgap(5);
+        gridPane.setHgap(5);
+        gridPane.setStyle("-fx-background-color: #C0C0C0");
+
+        gridPane.setAlignment(Pos.CENTER);
 
         gridPane.add(emailLabel,0,0);
         gridPane.add(emailTextField,1,0);
